8b2250f3fd5cf96efdb3ed5e6dfd3ae8dfc7e6ef
xakar320 <xakar2@gmail.com>
2026-06-05 13:47:45 +0100

    Update balance.js

--- api/balance.js
+++ api/balance.js
@@ -1,1 +1,29 @@
-api/balance.js
+export default async function handler(req, res) {
+
+  try {
+
+    const response = await fetch("https://autosyncng.com/api/profile", {
+
+      method: "GET",
+
+      headers: {
+        Authorization: "Bearer 988|WDYTnvBrrSIlZoflQMuU8lLRotdLUInb6c989515",
+        Accept: "application/json"
+      }
+
+    });
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      status: "error",
+      message: error.message
+    });
+
+  }
+
+}
