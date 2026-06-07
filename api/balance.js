6d66c9fe99577fc67d8f177e9f320998b4b82056
xakar320 <xakar2@gmail.com>
2026-06-07 05:51:10 +0100

    Update balance.js

--- api/balance.js
+++ api/balance.js
@@ -1,26 +1,54 @@
 export default async function handler(req, res) {
 
-  try {
+    try {
 
-    const response = await fetch("https://autosyncng.com/api/reseller/", {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer 988|WDYTnvBrrSIlZoflQMuU8lLRotdLUInb6c989515",
-        Accept: "application/json"
-      }
-    });
+        const response = await fetch(
+            "https://autosyncng.com/api/user", {
 
-    const data = await response.json();
+            method: "GET",
 
-    res.status(200).json(data);
+            headers: {
+                Authorization: "Bearer SAKA_API_TOKEN_DINKA",
+                Accept: "application/json"
+            }
 
-  } catch (error) {
+        });
 
-    res.status(500).json({
-      status: "error",
-      message: error.message
-    });
+        const data = await response.json();
 
-  }
+        // idan balance yana cikin wallet
+        if (data.wallet && data.wallet.balance) {
+
+            res.status(200).json({
+                balance: data.wallet.balance
+            });
+
+        }
+
+        // idan balance yana direct
+        else if (data.balance) {
+
+            res.status(200).json({
+                balance: data.balance
+            });
+
+        }
+
+        else {
+
+            res.status(200).json({
+                balance: 0
+            });
+
+        }
+
+    } catch (error) {
+
+        res.status(500).json({
+            status: "error",
+            message: error.message
+        });
+
+    }
 
 }
